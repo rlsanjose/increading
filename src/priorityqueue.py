16e3ddbbca05c6943114193f76d5afd63f552d1a
rlsanjose <rodrileja@gmail.com>
2024-12-22 23:18:58 +0100

    Change method name to add clarity

--- src/priorityqueue.py
+++ src/priorityqueue.py
@@ -1,26 +1,26 @@
 from operator import attrgetter
-import database, file_manager
+import database
+import file_manager
 
 
 class Priority_queue:
 
     def __init__(self):
         self.priority_list = []
-    
 
     def add_item(self, item):
         self.priority_list.append(item)
 
     def order_list(self):
-        
-        if len(self.priority_list) == 0 :
+
+        if len(self.priority_list) == 0:
             return
-        
+
         self.priority_list.sort(key=attrgetter("priority_percentage"))
-    
+
     # Retrieve list of materials and extracts from database
 
-    def retrieve_list_of_materials(self):
+    def retrieve_list_of_items(self):
         fm = file_manager.FileManager()
         db = database.Database(fm)
         material_list = db.read_all_materials()
